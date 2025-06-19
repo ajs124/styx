37cfa8f8df9275cd93036de71c5c5022d236c504
ajs124 <git@ajs124.de>
2025-06-19 16:09:04 +0200

    pkgs: drop default system

--- pkgs.nix
+++ pkgs.nix
@@ -2,12 +2,7 @@ let
   lock = builtins.fromJSON (builtins.unsafeDiscardStringContext (builtins.readFile (toString ./flake.lock)));
 
   inputs = {
-    nixpkgs = import (builtins.fetchTree lock.nodes.nixpkgs.locked) {
-      system =
-        builtins.currentSystem
-        # currently needed for (pure) tests -> only work on that platfrom
-        or "x86_64-linux";
-    };
+    nixpkgs = import (builtins.fetchTree lock.nodes.nixpkgs.locked) {};
     self = ./.;
   };
 
